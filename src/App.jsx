cc4ac2434325a221b0ea4ee6bfebcd8e09f2ff48
zainab449 <zainabmemon449@gmail.com>
2024-10-12 18:24:05 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ function App() {
 
   return (
     <div className="app">
- <h1>MovieLand</h1>
+ <h1>MovieWorld</h1>
       <div className="search">
         <input
           placeholder="Search for movies"
